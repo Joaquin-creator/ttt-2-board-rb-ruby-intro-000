d2dbb0d698b4f4acd2e6e01c90626ec7833aff3d
Joaquin Esquilin <esquilin.joaquin@gmail.com>
2020-01-12 17:29:40 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,19 @@
-   |   |
------------
-   |   |
------------
-   |   |
+#   |   |
+# -----------
+#   |   |
+# -----------
+#   |   |
 
-row = ["1", "2", "3"]
+# row = ["1", "2", "3"]
 
-# Position 1: Left
-row[0]
+# # Position 1: Left
+# row[0]
 
-# Position 2: Middle
-row[1]
+# # Position 2: Middle
+# row[1]
 
-# Position 3: Right
-row[2]
+# # Position 3: Right
+# row[2]
 
-# Move X to the Position 2, Middle
-row[1] = "x"
+# # Move X to the Position 2, Middle
+# row[1] = "x"
